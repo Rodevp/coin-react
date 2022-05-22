3156b6c2b5c96c9848f98990bf2e9c1c44c75ee8
Rodevp <rodriguezrodrigor12@gmail.com>
2022-05-22 12:34:47 -0500

    componentizando card

---
+++ coin/src/components/card/card.tsx
@@ -0,0 +1,51 @@
+import { useEffect } from 'react'
+
+import './card.css'
+
+interface Props {
+    image: string
+    name: string
+    price: number
+    symbol: string
+}
+
+function Card({ image, name, price, symbol }: Props) {
+
+    useEffect(() => {
+
+        const cards: NodeListOf<Element> = document.querySelectorAll('#card')
+        const allCards: Array<Element> = Array.from(cards)
+        
+        const idTimeOut = setTimeout(() => {
+
+            allCards.forEach(card => {
+                card.classList.add('fade_in')
+            })
+
+        }, 1000)
+
+        return () => {
+            clearTimeout(idTimeOut)
+        }
+
+    }, [])
+
+    return (
+        <div className='card' id='card'>
+            <section className='card_header'>
+                <img src={image} alt='logo' />
+                <p className='card_title'>
+                    {name}
+                    <span>
+                        {symbol}
+                    </span>
+                </p>
+            </section>
+            <section className='card_desc'>
+                <h3>Precio - {price}</h3>
+            </section>
+        </div>
+    )
+}
+
+export default Card
--- coin/src/screens/home.tsx
+++ coin/src/screens/home.tsx
@@ -1,33 +1,10 @@
+import Card from '../components/card/card'
 import TableCoins from '../components/table/table-coins'
-import { useEffect } from 'react'
+
 
 import './home.css'
 
 function Home() {
-
-
-    useEffect(() => {
-
-        const cards: NodeListOf<Element> = document.querySelectorAll('#card')
-        const allCards: Array<Element> = Array.from(cards)
-        
-        console.log(allCards)
-
-        const idTimeOut = setTimeout(() => {
-
-            allCards.forEach(card => {
-                card.classList.add('fade_in')
-            })
-
-        }, 1000)
-
-        return () => {
-            clearTimeout(idTimeOut)
-        }
-
-
-    }, [])
-
 
     return (
         <main>
@@ -41,20 +18,13 @@ function Home() {
                 <div className='content_top_4'>
                     {
                         [1, 2, 3, 4].map((n) => (
-                            <div className='card' id='card'>
-                                <section className='card_header'>
-                                    <img src='https://cdn.coinranking.com/bOabBYkcX/bitcoin_btc.svg' alt='a' />
-                                    <p className='card_title'>
-                                        name
-                                        <span>
-                                            symbol
-                                        </span>
-                                    </p>
-                                </section>
-                                <section className='card_desc'>
-                                    <h3>Precio - 43,000.01</h3>
-                                </section>
-                            </div>
+                            <Card 
+                                key={n}
+                                name='Bitcoin'
+                                image='https://cdn.coinranking.com/bOabBYkcX/bitcoin_btc.svg'
+                                symbol='BTC'
+                                price={20000}
+                            />
                         ))
                     }
                 </div>
